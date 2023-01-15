2f64e2eaeb533f6e30b94a3ab34660baee341b40
whyalwaysme <coworasavanila@gmail.com>
2023-01-15 21:17:01 +0700

    Dashboard

---
+++ resources/js/Pages/Dashboard.jsx
@@ -0,0 +1,28 @@
+import {useState} from 'react';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head } from '@inertiajs/inertia-react';
+import { Inertia } from '@inertiajs/inertia';
+
+export default function Dashboard(props) {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [category, setCategory] = useState('');
+    const [isNotif, setIsNotif] = useState(false);
+
+    
+    return (
+        <AuthenticatedLayout
+            auth={props.auth}
+            errors={props.errors}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Create News</h2>}
+        >
+            <Head title="My News" />
+
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
