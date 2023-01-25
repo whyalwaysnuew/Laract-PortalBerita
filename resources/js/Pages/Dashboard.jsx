433c0b269fe28c6f9ebd7ff08267a4b1b0323d65
whyalwaysme <coworasavanila@gmail.com>
2023-01-25 21:32:25 +0700

    t

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,26 +1,29 @@
-import {useState} from 'react';
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/inertia-react';
-import { Inertia } from '@inertiajs/inertia';
+import { useState } from "react";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/inertia-react";
+import { Inertia } from "@inertiajs/inertia";
 
 export default function Dashboard(props) {
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [category, setCategory] = useState('');
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [category, setCategory] = useState("");
     const [isNotif, setIsNotif] = useState(false);
 
-    
     return (
         <AuthenticatedLayout
             auth={props.auth}
             errors={props.errors}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Create News</h2>}
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    Create News
+                </h2>
+            }
         >
             <Head title="My News" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    abvvzzqc
+                    abvvzzqtc
                 </div>
             </div>
         </AuthenticatedLayout>
