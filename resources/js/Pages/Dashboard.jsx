cd1f4a66db06ae0ff12336313c65c89b02eaf54a
whyalwaysme <coworasavanila@gmail.com>
2023-01-27 21:22:42 +0700

    L

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -23,7 +23,7 @@ export default function Dashboard(props) {
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    abvvzzqatc
+                    abvvzzqatcL
                 </div>
             </div>
         </AuthenticatedLayout>
