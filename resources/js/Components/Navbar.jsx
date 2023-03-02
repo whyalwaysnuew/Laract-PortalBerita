2b449d0acadb2cb84b97643fa0907303d13c1828
whyalwaysme <coworasavanila@gmail.com>
2023-03-02 20:13:41 +0700

    --

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = ({ user }) => {
                                 >
                                     Dashboard
                                     <span className="badge badge-secondary">
-                                        New fzaazz
+                                        New fz
                                     </span>
                                 </Link>
                             </li>
