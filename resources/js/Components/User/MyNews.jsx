7809ed8738981d5becc61bedbd43abeda69ce551
whyalwaysme <coworasavanila@gmail.com>
2023-03-27 20:21:36 +0700

    -

--- resources/js/Components/User/MyNews.jsx
+++ resources/js/Components/User/MyNews.jsx
@@ -1,27 +1,25 @@
 const MyNews = () => {
     // return news.map((data, i) => {
-        return 
-        <div  className="card w-full lg:w-96 bg-base-100 shadow-xl">
-            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">
+    return;
+    <div className="card w-full lg:w-96 bg-base-100 shadow-xl">
+        <figure>
+            <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+        </figure>
+        <div className="card-body">
+            <h2 className="card-title">
                 {/* {data.title} */}
-                    <div className="badge badge-secondary">NEW</div>
-                </h2>
-                    <p>
-                        {/* {data.description} */}
-                    </p>
-                <div className="card-actions justify-end">
-                    <div className="badge badge-outline">
-                        {/* {data.category} */}
-                    </div> 
-                    <div className="badge badge-outline">
-                        {/* {data.author} */}
-                    </div>
+                <div className="badge badge-secondary">NEWs</div>
+            </h2>
+            <p>{/* {data.description} */}</p>
+            <div className="card-actions justify-end">
+                <div className="badge badge-outline">
+                    {/* {data.category} */}
                 </div>
+                <div className="badge badge-outline">{/* {data.author} */}</div>
             </div>
         </div>
+    </div>;
     // });
-}
+};
 
-export default MyNews
+export default MyNews;
