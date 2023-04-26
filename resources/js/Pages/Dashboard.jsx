c8ff0504c902ccf821d198ffa818044e9c4280ab
whyalwaysme <coworasavanila@gmail.com>
2023-04-26 20:20:31 +0700

    -

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -8,7 +8,7 @@ export default function Dashboard(props) {
             errors={props.errors}
             header={
                 <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-                    Create News
+                    Create News-
                 </h2>
             }
         >
